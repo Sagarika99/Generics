98c43afd736ba1e310e03df095362d5fabea01ef
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-21 00:43:31 +0530

    Added more than 3 parameters to generic

--- src/com/bridgelabz/generics/Maximumof3.java
+++ src/com/bridgelabz/generics/Maximumof3.java
@@ -2,20 +2,22 @@ package com.bridgelabz.generics;
 
 public class Maximumof3 <T extends Comparable<T>> {
 
-	T x , y , z;
+	T x , y , z, p, s;
 			
-	public Maximumof3(T x, T y, T z) {
+	public Maximumof3(T x, T y, T z, T p, T s) {
 		
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		this.p = p;
+		this.s = s;
 	}
 	
 	public void MaximumTest() {
-		MaximumTest(x,y,z);
+		MaximumTest(x,y,z,p,s);
 	}
 
-	public static <T extends Comparable<T>> void MaximumTest(T x, T y, T z) {
+	public static <T extends Comparable<T>> void MaximumTest(T x, T y, T z, T p, T s) {
 		T max = x;
 		if(y.compareTo(max)>0) {
 			max = y;
@@ -23,20 +25,27 @@ public class Maximumof3 <T extends Comparable<T>> {
 		if (z.compareTo(max)>0) {
 			max = z;
 		}
+		if (p.compareTo(max)>0) {
+			max = p;
+		}
+		if (s.compareTo(max)>0) {
+			max = s;
+		}
+		
 		System.out.println(max);
 	}
 	
 
 	public static void main(String[] args) {
-		Integer x1=15, y1=10, z1=12;		
+		Integer x1=15, y1=10, z1=12, p1=45, s1=2;		
 
-		Float x2=15.1f, y2=72.3f, z2=15.6f;
+		Float x2=15.1f, y2=72.3f, z2=15.6f, p2=5.5f, s2=100.0f;
 			
-		String x3="Apple", y3="Peach", z3="Banana";
+		String x3="Apple", y3="Peach", z3="Banana", p3="Pear", s3="Apricot";
 			
-		new Maximumof3(x1,y1,z1).MaximumTest();
-		new Maximumof3(x2,y2,z2).MaximumTest();
-		new Maximumof3(x3,y3,z3).MaximumTest();
+		new Maximumof3(x1,y1,z1,p1,s1).MaximumTest();
+		new Maximumof3(x2,y2,z2,p2,s2).MaximumTest();
+		new Maximumof3(x3,y3,z3,p3,s3).MaximumTest();
 		
 		
 		
