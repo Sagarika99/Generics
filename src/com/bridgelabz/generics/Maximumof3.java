8e002cd34eb42d9c43e59bab88e3f0e1f30f61ef
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-20 22:27:16 +0530

    maximum value out of 3 float numbers

--- src/com/bridgelabz/generics/Maximumof3.java
+++ src/com/bridgelabz/generics/Maximumof3.java
@@ -4,8 +4,12 @@ public class Maximumof3 {
 
 	public static void main(String[] args) {
 		Integer[] intarray = {15,10,12};
-		int integer = MaximumInt(intarray);
-		System.out.println("Max Integer: "+integer);
+		int integernum = MaximumInt(intarray);
+		System.out.println("Max Integer: "+integernum);
+		
+		Float[] floatarray = {15.1f,10.5f,15.6f};
+		Float floatnum = MaximumFloat(floatarray);
+		System.out.println("Max Float: "+floatnum);
 	}
 
 	public static int MaximumInt(Integer[] intarray) {
@@ -15,6 +19,17 @@ public class Maximumof3 {
 		}
 		if (intarray[2].compareTo(maxnum)>0) {
 			maxnum = intarray[2];
+		}
+		return maxnum;
+	}
+	
+	public static float MaximumFloat(Float[] floatarray) {
+		float maxnum = floatarray[0];
+		if(floatarray[1].compareTo(maxnum)>0) {
+			maxnum = floatarray[1];
+		}
+		if (floatarray[2].compareTo(maxnum)>0) {
+			maxnum = floatarray[2];
 		}
 		return maxnum;
 	}
