d59d234f6ab48f529760511af3d7c01c08fecc67
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-21 00:25:59 +0530

    Refractored the code to create generic class

--- src/com/bridgelabz/generics/Maximumof3.java
+++ src/com/bridgelabz/generics/Maximumof3.java
@@ -1,65 +1,46 @@
 package com.bridgelabz.generics;
 
-public class Maximumof3 {
+public class Maximumof3 <T extends Comparable<T>> {
 
-	public static void main(String[] args) {
-		Integer[] array1 = {15,10,12};
-		MaximumTest(array1);
+	T x , y , z;
+			
+	public Maximumof3(T x, T y, T z) {
 		
-		
-		Float[] array2 = {15.1f,10.5f,15.6f};
-		MaximumTest(array2);
-		
-		
-		String[] array3 = {"Apple", "Peach", "Banana"};
-		MaximumTest(array3);
-		
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public void MaximumTest() {
+		MaximumTest(x,y,z);
 	}
 
-	public static <T extends Comparable<T>> void MaximumTest(T[] array) {
-		T max = array[0];
-		if(array[1].compareTo(max)>0) {
-			max = array[1];
+	public static <T extends Comparable<T>> void MaximumTest(T x, T y, T z) {
+		T max = x;
+		if(y.compareTo(max)>0) {
+			max = y;
 		}
-		if (array[2].compareTo(max)>0) {
-			max = array[2];
+		if (z.compareTo(max)>0) {
+			max = z;
 		}
 		System.out.println(max);
 	}
 	
-	
-	
-/**	public static int MaximumInt(Integer[] intarray) {
-		int maxnum = intarray[0];
-		if(intarray[1].compareTo(maxnum)>0) {
-			maxnum = intarray[1];
-		}
-		if (intarray[2].compareTo(maxnum)>0) {
-			maxnum = intarray[2];
-		}
-		return maxnum;
+
+	public static void main(String[] args) {
+		Integer x1=15, y1=10, z1=12;		
+
+		Float x2=15.1f, y2=72.3f, z2=15.6f;
+			
+		String x3="Apple", y3="Peach", z3="Banana";
+			
+		new Maximumof3(x1,y1,z1).MaximumTest();
+		new Maximumof3(x2,y2,z2).MaximumTest();
+		new Maximumof3(x3,y3,z3).MaximumTest();
+		
+		
+		
 	}
-	
-	public static float MaximumFloat(Float[] floatarray) {
-		float maxnum = floatarray[0];
-		if(floatarray[1].compareTo(maxnum)>0) {
-			maxnum = floatarray[1];
-		}
-		if (floatarray[2].compareTo(maxnum)>0) {
-			maxnum = floatarray[2];
-		}
-		return maxnum;
-	}
-	
-	public static String MaximumString(String[] stringarray) {
-		String maxstring = stringarray[0];
-		if(stringarray[1].compareTo(maxstring)>0) {
-			maxstring = stringarray[1];
-		}
-		if (stringarray[2].compareTo(maxstring)>0) {
-			maxstring = stringarray[2];
-		}
-		return maxstring;
-	}**/
+
 }
 
